e9dc05b626798676f574bb1c94181ba68d5aa157
kamath-abhijith <kamath.abhijith@gmail.com>
2020-01-17 17:04:32 +0530

    global descriptors

--- TFApkg.py
+++ TFApkg.py
@@ -1,38 +1,109 @@
 import numpy as np
 
-def signal_moment(t,x,n):
-	'''
-	Computes the nth moment over the
-	density formed by the signal x
-	assuming it has unit norm
+def ctft(t,x):
+    '''
+    Computes the 'continuous' time Fourier
+    transform using the inbuilt fft function
+    to return the complex spectrum with fftshift
+    and the range of frequencies
 
-	INPUT:  Support vector, t
-		    Signal, x
-		    Order, n
-	OUTPUT: nth moment
+    INPUT:  Time support, t
+            Signal, x
+    OUTPUT: Frequency support, w
+            Complex Fourier transform, x_hat
 
-	Author: Abijith J Kamath
-	kamath-abhijith.github.io
-	'''
-	dt = t[1]-t[0]
-	e_density = np.abs(x)**2
-	weights = t**n
+    Author: Abijith J Kamath
+    kamath-abhijith.github.io
+    '''
+    dt = t[1]-t[0]
+    
+    x_hat = np.fft.fft(x)
+    w = np.fft.fftfreq(x.size)*2*np.pi/dt
+    x_hat *= dt*np.exp(-1j*w*t[0])/(np.sqrt(2*np.pi))
 
-	return np.dot(weights,e_density)*dt
+    return w, x_hat
+
+def signal_moment(t,x,n=1,*argv):
+    '''
+    Computes the nth moment over the
+    density formed by the signal x
+    assuming it has unit norm
+
+    INPUT:  Support vector, t
+            Signal, x
+            Order, n
+    OUTPUT: nth moment
+
+    Author: Abijith J Kamath
+    kamath-abhijith.github.io
+    '''
+    if len(argv)>0:
+    	g = argv[0]
+    else:
+    	g = t
+
+    dt = t[1]-t[0]
+    e_density = np.abs(x)**2
+    weights = g**n
+
+    return np.dot(weights,e_density)*dt
 
 def signal_spread(t,x):
-	'''
-	Computes the spread of the signal
-	x assuming it has unit norm
+    '''
+    Computes the spread of the signal
+    x assuming it has unit norm
 
-	INPUT:  Support vector, t
-		    Signal, x
-	OUTPUT: Spread
+    INPUT:  Support vector, t
+            Signal, x
+    OUTPUT: Spread
 
-	Author: Abijith J Kamath
-	kamath-abhijith.github.io
-	'''
-	_mean = signal_moment(t,x,1)
-	_var = signal_moment(t,x,2)
+    Author: Abijith J Kamath
+    kamath-abhijith.github.io
+    '''
+    _mean = signal_moment(t,x)
+    _var = signal_moment(t,x,2)
 
-	return _var - _mean**2
+    return _var - _mean**2
+
+def inst_frequency(t,x):
+    '''
+    Computes the instantaneous
+    frequencies of the signal x
+
+    INPUT:  Support vector, t
+            Signal, x
+    OUTPUT: Shrunk time support, t
+            Instantaneous frequency, inst_freq
+
+    Author: Abijith J Kamath
+    kamath-abhijith.github.io
+    '''
+    dt = t[1]-t[0]
+    N = len(t)
+    t = t[0:N-1]
+    
+    inst_phase = np.unwrap(np.angle(x))
+    inst_freq = np.diff(inst_phase)/(dt)
+    
+    return t, inst_freq
+
+def group_delay(t,x):
+    w,x_hat = ctft(t,x)
+    dw = w[1]-w[0]
+    N = len(w)
+    w = w[0:N-1]
+
+    phase_spectra = np.unwrap(np.angle(x_hat))
+    group_delay = -1*np.diff(phase_spectra)/(dw)
+
+    return w, group_delay
+
+def signal_covariance(t,x):
+    N = len(x)
+    w,x_hat = ctft(t,x)
+    td,inst_freq = inst_frequency(t,x)
+    
+    t_mean = signal_moment(t,x,1)
+    w_mean = signal_moment(w,x_hat,1)
+    cov = signal_moment(t,x[0:N-1],1,td*inst_freq)
+    return cov - t_mean*w_mean
---
+++ main.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib import style
+from matplotlib import rcParams
+
+from TFApkg import *
+
+## Support
+dt = 0.01
+t = np.arange(-10,10,dt)
+
+## Cubic phase with Gaussian envelope
+# Signal Parameters
+a = 1
+b = 4
+c = 0
+w0 = 2
+
+# Time-domain representation
+s = (a/np.pi)**(1/4)*np.exp(-a*t**2/2 + 1j*c*t**3/3 + 1j*b*t**2/2 + 1j*w0*t)
+
+# Frequency domain representation
+w,s_hat = ctft(t,s)
+
+## Global averages
+t_mean = signal_moment(t,s)
+duration = signal_spread(t,s)	
+
+w_mean = signal_moment(w,s_hat)
+bandwidth = signal_spread(w,s_hat)
+
+print('Temporal mean and duration: ',t_mean,duration)
+print('Spectral mean and bandwidth: ',w_mean,bandwidth)
+
+## Instantaneous frequency and group delay
+td, wi = inst_frequency(t,s)
+wd, tg = group_delay(t,s)
+
+## Covariance
+cov_s = signal_covariance(t,s)
+print('Signal covariance: ',cov_s)
+
+## Plots
+style.use('ggplot')
+style.use('dark_background')
+
+rcParams['text.usetex'] = True
+
+fig, plts = plt.subplots(2,2)
+plts[0][0].plot(t,s.real)
+plts[0][0].scatter(t_mean,0,color='red')
+plts[0][0].set_ylabel(r'$\Re(s)$')
+plts[0][0].set_xlabel(r'time (sec)')
+plts[1][0].plot(t,s.imag)
+plts[1][0].scatter(t_mean,0,color='red')
+plts[1][0].set_ylabel(r'$\Im(s)$')
+plts[1][0].set_xlabel(r'time (sec)')
+
+plts[0][1].plot(w,s_hat.real)
+plts[0][1].scatter(w_mean,0,color='red')
+plts[0][1].set_ylabel(r'$\Re(\hat{s})$')
+plts[0][1].set_xlabel(r'Frequency (Hz)')
+plts[0][1].set_xlim((-30,30))
+plts[1][1].plot(w,s_hat.imag)
+plts[1][1].scatter(w_mean,0,color='red')
+plts[1][1].set_ylabel(r'$\Im(\hat{s})$')
+plts[1][1].set_xlabel(r'Frequency (Hz)')
+plts[1][1].set_xlim((-30,30))
+plt.title('Time and Frequency Descriptions')
+
+plt.figure()
+plt.plot(td,wi,'-',color='red',label='Estimate')
+plt.plot(t,w0 + b*t + c*t**2,'--',color='green',label='True')
+plt.ylabel(r'$\omega_i(t)$')
+plt.xlabel(r'time (sec)')
+plt.title('Instantaneous Frequency')
+plt.legend()
+
+plt.figure()
+plt.plot(wd,tg,'-',color='red',label='Estimate')
+plt.plot(t,(b/(a**2+b**2))*(w-w0),'--',color='green',label='True')
+plt.ylabel(r'$t_g(\omega)$')
+plt.xlabel(r'Frequency (Hz)')
+plt.title('Group Delay')
+plt.legend()
+plt.show()
